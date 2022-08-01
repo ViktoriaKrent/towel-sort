e0837494a21a8ad350e6be0f119f8bbafe851f87
ViktoriaKrent <mrs.victoria.krent.99@gmail.com>
2022-08-01 20:35:11 +0300

    feat:create function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) {
+        return [];
+    }
+    matrix.forEach((e, i) =>
+        i % 2 === 0 ? e.sort((a, b) => a - b) : e.sort((a, b) => b - a)
+    );
+    return matrix.reduce((acc, e) => {
+        acc.push(...e);
+        return acc;
+    }, []);
+};
